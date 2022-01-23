a0553dcd0bee9fc5c79ffc2d48df48d2c0a5edf7
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2022-01-23 19:01:13 +0000

    PROTOTYPE: extracting time in specific time

---
+++ teste.py
@@ -0,0 +1,65 @@
+import time
+import requests
+import datetime
+from bs4 import BeautifulSoup
+
+while True:
+  localtime = time.localtime()
+  result = time.strftime("%H:%M", localtime)
+  print(result)
+  
+  if result == "18:52":
+    #Executar report tempo!
+    
+    #Store time
+    time = result
+    
+    #Text to be in report
+    text = ""
+    text += time + "\n"
+    #print (time)        
+    
+    #Read from internet
+    #Get info from URL
+    url = 'https://www.tempo.pt/madeira-provincia.htm'
+    result = requests.get(url) #GET request
+    
+    doc = BeautifulSoup(result.text, "html.parser")
+
+    
+    #Tempo madeira
+    tempBox = doc.find("ul", {"class":"ul-top-prediccion top-pred"})
+    tempPredict = tempBox.find_all("li", {"class":"li-top-prediccion"})    
+
+    for temperatura in tempPredict:
+        lugar = temperatura.find("a", {"class":"anchors"})
+        maxTemp = temperatura.find("span", {"class": "cMax changeUnitT"})
+        minTemp = temperatura.find("span", {"class": "cMin changeUnitT"})
+        text += str(lugar.string) + " Max: " + str(maxTemp.string) + " Min: " + str(minTemp.string) + "\n"
+    
+    print(text)
+    
+    #To open local html file:
+    with open("report.txt", "w") as f:
+        f.write(text)
+        print("Success report!")    
+    
+    print("tempoooo")
+  #time.sleep(1)
+
+"""
+for temperatura in tempPredict:
+    lugar = temperatura.find("a", {"class":"anchors"})
+    maxTemp = temperatura.find("span", {"class": "cMax changeUnitT"})
+    minTemp = temperatura.find("span", {"class": "cMin changeUnitT"})
+    text += str(lugar.string) + " Max: " + str(maxTemp.string) + " Min: " + str(minTemp.string) + "\n"
+
+print(text)
+
+#To open local html file:
+with open("report.txt", "w") as f:
+    f.write(text)
+    print("Success report!")
+
+"""
+
