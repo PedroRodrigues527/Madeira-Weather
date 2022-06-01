e8e78a4ca95d2c01fc0931a395c1968cc54d84d8
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2022-06-01 20:57:05 +0100

    Refactoring

--- weather.py
+++ weather.py
@@ -4,6 +4,7 @@ Description: Retrieve weather information about Madeira
              Island using python (Webscraping);
 2022
 """
+from typing import List
 
 import requests
 import datetime
@@ -11,54 +12,63 @@ import yagmail
 from os import system, name
 from bs4 import BeautifulSoup
 
-url = ['https://www.tempo.pt/calheta.htm','https://www.tempo.pt/camara-de-lobos.htm','https://www.tempo.pt/funchal.htm','https://www.tempo.pt/machico.htm','https://www.tempo.pt/ponta-do-sol.htm','https://www.tempo.pt/porto-moniz.htm','https://www.tempo.pt/ribeira-brava.htm','https://www.tempo.pt/santa-cruz_madeira-l32099.htm','https://www.tempo.pt/santana.htm','https://www.tempo.pt/sao-vicente.htm']
+url = ['https://www.tempo.pt/calheta.htm', 'https://www.tempo.pt/camara-de-lobos.htm',
+       'https://www.tempo.pt/funchal.htm', 'https://www.tempo.pt/machico.htm', 'https://www.tempo.pt/ponta-do-sol.htm',
+       'https://www.tempo.pt/porto-moniz.htm', 'https://www.tempo.pt/ribeira-brava.htm',
+       'https://www.tempo.pt/santa-cruz_madeira-l32099.htm', 'https://www.tempo.pt/santana.htm',
+       'https://www.tempo.pt/sao-vicente.htm']
 
-lugares = ["Calheta","Camara de Lobos","Funchal","Machico","Ponta de Sol","Porto Moniz","Ribeira Brava","Santa Cruz","Santana","São Vicente"]
+lugares: list[str] = ["Calheta", "Camara de Lobos", "Funchal", "Machico", "Ponta de Sol", "Porto Moniz",
+                      "Ribeira Brava", "Santa Cruz", "Santana", "São Vicente"]
+
+
+def showLugares():
+    for lugar in range(len(lugares) - 1):
+        print(str(lugar) + "- " + lugares[lugar])
 
 def places():
     clean()
     print("***ESPECIFICO***")
-    index = 1
-    for lugar in range(len(lugares)-1):
-        print(str(index) + "- "+ lugares[lugar])
-        index += 1
-    op = input("")
-    
-    #ATUAL DE LUGAR
+    showLugares()
+    city = input("")
+
     try:
-        print("");
-        print("Info: "+ lugares[int(op)-1])
-        atualtempEsp(lugares[int(op)-1])
-    except:
+        print("")
+        print("Info: " + lugares[int(city) - 1])
+        atualTempoEspecifico(lugares[int(city) - 1])
+    except IndexError:
         print("Utilize opção correta e apenas caracteres numericos")
-        
-def atualtempEsp(lugarStr):
-    tempBox = connect('https://www.tempo.pt/madeira-provincia.htm').find("ul", {"class":"ul-top-prediccion top-pred"})
-    tempPredict = tempBox.find_all("li", {"class":"li-top-prediccion"})  
+        places()
+
+
+def atualTempoEspecifico(place):
+    tempBox = connect('https://www.tempo.pt/madeira-provincia.htm').find("ul", {"class": "ul-top-prediccion top-pred"})
+    tempPredict = tempBox.find_all("li", {"class": "li-top-prediccion"})
     time()
     for temperatura in tempPredict:
-        lugar = temperatura.find("a", {"class":"anchors"})
-        if lugarStr == str(temperatura.find("a", {"class":"anchors"}).string):
+        lugar = temperatura.find("a", {"class": "anchors"})
+        if place == str(temperatura.find("a", {"class": "anchors"}).string):
             maxTemp = temperatura.find("span", {"class": "cMax changeUnitT"})
             minTemp = temperatura.find("span", {"class": "cMin changeUnitT"})
             print(str(lugar.string) + " Max: " + str(maxTemp.string) + " Min: " + str(minTemp.string))
     print("")
     init()
 
+
 def connect(link):
-    url = link
-    result = requests.get(url)
+    result = requests.get(link)
     doc = BeautifulSoup(result.text, "html.parser")
     return doc
 
-def atualtemp():
-    tempBox = connect('https://www.tempo.pt/madeira-provincia.htm').find("ul", {"class":"ul-top-prediccion top-pred"})
-    tempPredict = tempBox.find_all("li", {"class":"li-top-prediccion"})
-    
+
+def atualTempo():
+    tempBox = connect('https://www.tempo.pt/madeira-provincia.htm').find("ul", {"class": "ul-top-prediccion top-pred"})
+    tempPredict = tempBox.find_all("li", {"class": "li-top-prediccion"})
+
     time()
-    text = [];
+    text = []
     for temperatura in tempPredict:
-        lugar = temperatura.find("a", {"class":"anchors"})
+        lugar = temperatura.find("a", {"class": "anchors"})
         maxTemp = temperatura.find("span", {"class": "cMax changeUnitT"})
         minTemp = temperatura.find("span", {"class": "cMin changeUnitT"})
         print(str(lugar.string) + " Max: " + str(maxTemp.string) + " Min: " + str(minTemp.string))
@@ -66,86 +76,105 @@ def atualtemp():
         text.append(textStr)
     print("")
     saveToFile(text, "weather.txt")
+    time()
+    #init()
+
+
+def weekTempo():
+    print("*** TEMPO ATE 7 DIAS ***")
+    text = []
+    for i in range(len(url)):
+        try:
+            print(lugares[i])
+            text.append("****\n" + lugares[i] + "\n")
+            tempBox = connect(url[i]).find("span", {"class": "datos-dos-semanas"})
+            liTemp = tempBox.find_all("li")
+            day_of_the_week = 0  # control of week
+            for li in liTemp:
+                dia = li.find("span", {"class": "cuando"})
+                Temp = li.find("span", {"class": "temperatura"})
+                maxTemp = Temp.find("span", {"class": "maxima changeUnitT"})
+                minTemp = Temp.find("span", {"class": "minima changeUnitT"})
+                textStr = str(dia.text) + " Max:" + str(maxTemp.string) + " Min:" + str(minTemp.string) + "\n"
+                print(textStr)
+                text.append(textStr)
+                day_of_the_week += 1
+                if day_of_the_week == 7: #Fim da semana
+                    text.append("\n")
+                    print("")
+                    break
+            saveToFile(text, "weather.txt")
+        except FileExistsError:
+            print("Error saving to file!")
+    print("")
     init()
 
-def amanhaTemp():
-    print("*** TEMPO ATE 7 DIAS ***")
-    text = [];
-    for i in range(len(url)):
-        print(lugares[i])
-        text.append("****\n"+lugares[i]+"\n")
-        tempBox = connect(url[i]).find("span", {"class":"datos-dos-semanas"})
-        liTemp = tempBox.find_all("li")  
-        i = 0 #control of week
-        #text = [];
-        for li in liTemp:
-            dia = li.find("span",{"class": "cuando"})
-            
-            Temp = li.find("span", {"class": "temperatura"})
-            maxTemp = Temp.find("span", {"class": "maxima changeUnitT"})
-            minTemp = Temp.find("span", {"class": "minima changeUnitT"})
-            print(str(dia.text)+" Max:"+str(maxTemp.string)+" Min:"+str(minTemp.string))
-            textStr = str(dia.text)+" Max:"+str(maxTemp.string)+" Min:"+str(minTemp.string) + "\n"
-            text.append(textStr)
-            i+=1
-            if i == 7:
-                text.append("\n")
-                print("")
-                break
-        saveToFile(text, "weather.txt")
-    print("")
-    init()    
-    
+
 def saveToFile(content, file):
-    
-    with open(file, 'w') as f:
-        for strings in content:
-            f.write(strings)
-            #f.write("\n")
-        print("Content saved with success")
-    
-        
-#cleaning console
+    try:
+        with open(file, 'w') as f:
+            for strings in content:
+                f.write(strings)
+            print("Content saved with success")
+    except FileExistsError as e:
+        print("Error opening the file\n"+e)
+    except FileNotFoundError as e:
+        print("File not Found\n"+e)
+
+# cleaning console
 def clean():
     if name == 'nt':
         system('cls')
     else:
         system('clear')
 
+
 def sendEmail():
-    userEmail = input("Insira o seu email: ")
-    yag = yagmail.SMTP('xxx@gmail.com', 'xxx')
-    yag.send(userEmail, 'Weather Report', "Weather Report", 'weather.txt')
-    print("Enviando por email...")
-    print("Enviado")
+    try:
+        user_email = input("Insira o seu email: ")
+        yag = yagmail.SMTP('lantransferwebsite@gmail.com', 'webserver123')
+        yag.send(user_email, 'Weather Report', "Weather Report", 'weather.txt')
+        print("Enviando por email...")
+        print("Enviado")
+    except ConnectionError as e:
+        print("Error while sending the email.\n"+e)
+    except ConnectionResetError as e:
+        print("Error while connecting the email.\n" + e)
+    except ConnectionAbortedError as e:
+        print("Error connection aborted\n" + e)
+    except ConnectionRefusedError as e:
+        print("Error connection refused.\n" + e)
+
 
 def time():
     today = datetime.datetime.now()
-    print (today.strftime("%Y-%m-%d %H:%M:%S"))
+    print(today.strftime("%Y-%m-%d %H:%M:%S.%f"))
 
 
-def init():
+def showMainMenu():
     print("***METEOROLOGIA***")
     print("1- ATUAL")
     print("2- HOJE ATE 7 DIAS")
     print("3- ESPECIFICO")
     print("4- Enviar por email")
     print("")
-    op = input("")
-    
-    try:
-        if op == '1':     
-            atualtemp()
-        elif op == '2':  
-            amanhaTemp()
-        elif op == '3':
-            places()
-        elif op == '4':
-            sendEmail()
-    except:
 
-        print("Option not valid\nUse only numerical caracters between 1 and 3")
+def init():
+    showMainMenu()
+    op = input("")
+
+    if op == '1':
+        atualTempo()
+    elif op == '2':
+        weekTempo()
+    elif op == '3':
+        places()
+    elif op == '4':
+        sendEmail()
+    else:
+        print("Option not valid\nUse only numerical characters between 1 and 3")
         init()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     init()
